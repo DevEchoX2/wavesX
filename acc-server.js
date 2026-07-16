bcdc69bd1ee6813ef2dbe238b3c3dc35616f5351
DevvyE_yo <DevEchoX@gmail.com>
2026-07-15 21:36:21 -0400

    Update acc-server.js

--- acc-server.js
+++ acc-server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const DATA_DIR = path.join(__dirname, 'data');
 const DB_FILE = path.join(DATA_DIR, 'users.json');
